d03022b009e7b1441de55c05e524f2faae639747
Vicente Rivera <vicenterivera225@gmail.com>
2024-12-27 21:10:38 -0300

    postgresql, prisma y docker

---
+++ prisma/migrations/20241227205756_dev/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Date" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "start" TIMESTAMP(3) NOT NULL,
+    "end" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Date_pkey" PRIMARY KEY ("id")
+);
