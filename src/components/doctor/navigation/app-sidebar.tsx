80e02958316494215cf511a6e61d80e4eb5d7058
Vicente Rivera <vicenterivera225@gmail.com>
2024-12-28 21:05:48 -0300

    migracion a shadcn sidebar

---
+++ src/components/doctor/navigation/app-sidebar.tsx
@@ -0,0 +1,79 @@
+import Link from "next/link";
+import DoctorProfile from "./doctor-profile";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import {
+  Calendar,
+  HospitalIcon,
+  LayoutDashboardIcon,
+  SquareActivityIcon,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+const items = [
+  {
+    title: "Dashboard",
+    url: "/doctor",
+    icon: <LayoutDashboardIcon className="size-4" />,
+  },
+  {
+    title: "Agenda",
+    url: "/doctor/agenda",
+    icon: <Calendar className="size-4" />,
+  },
+  {
+    title: "Dates",
+    url: "/doctor/dates",
+    icon: <HospitalIcon className="size-4" />,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <div className="flex h-14 justify-between items-center px-4 lg:h-[60px] lg:px-6 text-black">
+          <Link href="/" className="flex items-center gap-2 font-semibold">
+            <SquareActivityIcon className="h-8 w-8" />
+            <span>Agenda Médica</span>
+          </Link>
+        </div>
+        <DoctorProfile />
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Menu</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      {item.icon}
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <Button>
+          Cerrar sesión
+        </Button>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
